ad3a3462116f68bc87913c777ef1856175928c0b
Muhammad-Nadeem786 <nadeemmomini77@gmail.com>
2024-09-25 01:49:06 -0700

    Basic js

---
+++ variables.js
@@ -0,0 +1,18 @@
+//In java script there are three type of variableb 
+//let 
+//var
+//const
+
+let a = 3;
+let b = 4;
+console.log(sun,a+b);
+
+var name = "Muhammad Nadeem";
+var age = 22;
+var regno = 5;
+console.log(name,age,regno);
+
+const num = 3;
+const num2 = 4;
+add = num + num2;
+console.log(add);
